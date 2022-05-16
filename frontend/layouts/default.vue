946a97f4130e0d49429193b2934387787a98d1bc
DonnaVolk <75436998+DonnaVolk@users.noreply.github.com>
2022-05-16 23:20:04 +0200

    frontend accountpage

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
+  <v-app light>
+    <v-navigation-drawer 
       v-model="drawer"
       :mini-variant="miniVariant"
       :clipped="clipped"
@@ -24,37 +24,17 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    <v-app-bar color="primary" :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon class="white--text" @click.stop="drawer = !drawer" />
+      <v-toolbar-title class="white--text" v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
+    <v-navigation-drawer class="white--text" v-model="rightDrawer" :right="right" temporary fixed>
     </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -72,20 +52,35 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/home',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          icon: 'mdi-briefcase-variant',
+          title: 'Aanbevolen banen',
+          to: '/recommended',
+        },
+        {
+          icon: 'mdi-bell-ring',
+          title: 'Meldingen',
+          to: '/notifications',
+        },
+         {
+          icon: 'mdi-account',
+          title: 'Profiel',
+          to: '/account',
+        },
+        {
+          icon: 'mdi-logout',
+          title: 'Uitloggen',
+          to: '/',
         },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'WerkZe',
     }
   },
 }
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -50,13 +50,14 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
+        light: {
+          primary: '#7A9E7E',
+          accent: '#31493C',
+          secondary: '#7A9E7E',
+          background: '#E8F1F2',
+          info: '#a4bfac',          
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
