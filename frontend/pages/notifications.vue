9557afc9e483495ea0b737bcef32600fe60c9f72
Levi Reijns <79833570+Levidevtech@users.noreply.github.com>
2022-06-05 14:13:02 +0200

    Added screens place holders for

---
+++ frontend/pages/notifications.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-row>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card class="mx-auto my-12" max-width="344">
+      <v-card-text>
+        <div>Melding van bedrijf:</div>
+        <p class="text-h4 text--primary">Kinderboerderij Het hofje</p>
+        <p><v-icon x-large> mdi-dog </v-icon></p>
+        <div class="text--primary">De leukste plek voor jong en oud!</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary accent-4"> Contact informatie </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'homePage',
+}
+</script>
---
+++ frontend/pages/recommended.vue
@@ -0,0 +1,400 @@
+<template>
+  <v-row>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+      <v-img
+        height="250"
+        src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+      ></v-img>
+
+      <v-card-title>Cafe Brownies&downieS</v-card-title>
+
+      <v-card-text>
+        <v-row align="center" class="mx-0">
+          <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+          ></v-rating>
+
+          <div class="grey--text ms-4">4.5 (413)</div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">$ • "Puur is perfect"</div>
+
+        <div>
+          De combinatie van pure, bijzondere medewerkers en ambachtelijke
+          gerechten om je vingers bij af te likken, maakt een bezoekje aan
+          Brownies&downieS tot een ware beleving. Puur genieten dus.
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-actions>
+        <v-btn color="primary lighten-2" text @click="reserve">
+          Vind ik leuk!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'homePage',
+}
+</script>
